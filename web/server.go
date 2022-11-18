f3023f4d32a8065bbdf994b9fe6632fbb3397fd9
Alex <1353716+alexstojda@users.noreply.github.com>
2022-11-17 23:30:25 -0500

    feat(server): Initialize new user/authn routes and controllers in server

--- web/server.go
+++ web/server.go
@@ -1,31 +1,38 @@
 package web
 
 import (
-	"gorm.io/gorm"
-	"pinman/web/api/health"
-	"pinman/web/api/hello"
-
+	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/logger"
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	ginprometheus "github.com/zsais/go-gin-prometheus"
+	"gorm.io/gorm"
+	"pinman/web/api/auth"
+	"pinman/web/api/health"
+	"pinman/web/api/hello"
+	"pinman/web/api/user"
 )
 
 type Server struct {
-	SPAPath string
-	GormDB  *gorm.DB
-	Health  *health.Health
-	Hello   *hello.Hello
+	ClientOrigin string
+	SPAPath      string
+	GormDB       *gorm.DB
+	Health       *health.Health
+	Hello        *hello.Hello
+	Auth         *auth.Controller
+	User         *user.Controller
 }
 
-func NewServer(spaPath string, gormDb *gorm.DB) *Server {
+func NewServer(spaPath string, clientOrigin string, gormDb *gorm.DB) *Server {
 	return &Server{
-		SPAPath: spaPath,
-		GormDB:  gormDb,
-		Health:  health.NewHealth(),
-		Hello:   hello.NewHello(),
+		ClientOrigin: clientOrigin,
+		SPAPath:      spaPath,
+		Health:       health.NewHealth(),
+		Hello:        hello.NewHello(),
+		Auth:         auth.NewController(gormDb),
+		User:         user.NewController(gormDb),
 	}
 }
 
@@ -36,6 +43,12 @@ func (s *Server) StartServer() {
 	if gin.IsDebugging() {
 		zerolog.SetGlobalLevel(zerolog.DebugLevel)
 	}
+
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowOrigins = []string{"http://localhost:8000", s.ClientOrigin}
+	corsConfig.AllowCredentials = true
+
+	router.Use(cors.New(corsConfig))
 
 	router.Use(logger.SetLogger(logger.Config{
 		SkipPath: []string{
@@ -50,7 +63,7 @@ func (s *Server) StartServer() {
 	prometheus.ReqCntURLLabelMappingFn = func(c *gin.Context) string {
 		url := c.Request.URL.Path // Query params are dropped here so there is not a metric for every permutation of query param usage on a route
 
-		//  If a route uses parameters, replace the parameter value with its name. Else there will be a metric for the route with
+		//  If a route uses parameters, replace the parameter value with its name. Else there will be a metric for the route
 		//  with every possible value of that parameter and this will cause performance issues in Prometheus.
 		//
 		//  If your service uses route parameters, uncomment the for loop below and add a case for each parameter. The example case
@@ -69,8 +82,13 @@ func (s *Server) StartServer() {
 	router.Use(errorHandler)
 
 	// API ROUTES
-	router.GET("/api/health", s.Health.Get)
-	router.GET("/api/hello", s.Hello.Get)
+	apiRouter := router.Group("/api")
+
+	apiRouter.GET("/health", s.Health.Get)
+	apiRouter.GET("/hello", s.Hello.Get)
+
+	auth.NewRouteController(s.Auth).AuthRoutes(apiRouter)
+	user.NewRouteController(s.User).UserRoutes(apiRouter)
 
 	// SPA ROUTE
 	// Only loaded if SPAPath is defined.
