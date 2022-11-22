0734963059834e58cfdfd3fb30e6d7300d43c310
alex <1353716+alexstojda@users.noreply.github.com>
2022-11-21 21:05:18 -0500

    feat(frontend): Setup react router

--- web/app/src/index.tsx
+++ web/app/src/index.tsx
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './pages/App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App/>} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
