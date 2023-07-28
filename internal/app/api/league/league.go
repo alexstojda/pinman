aa68da166bad5857ee62318b003d553611aa6b7a
Alex <1353716+alexstojda@users.noreply.github.com>
2023-07-28 10:14:45 -0400

    feat(leagues): Update league API to use Locations

--- internal/app/api/league/league.go
+++ internal/app/api/league/league.go
@@ -1,12 +1,14 @@
 package league
 
 import (
+	"errors"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog/log"
 	"gorm.io/gorm"
 	"net/http"
 	"pinman/internal/app/api/auth"
-	"pinman/internal/app/api/errors"
+	apierrors "pinman/internal/app/api/errors"
 	"pinman/internal/app/generated"
 	"pinman/internal/models"
 	"pinman/internal/utils"
@@ -29,46 +31,59 @@ func (c *Controller) CreateLeague(ctx *gin.Context) {
 
 	currentUser, err := auth.GetUser(ctx)
 	if err != nil {
-		errors.AbortWithError(http.StatusForbidden, err.Error(), ctx)
+		apierrors.AbortWithError(http.StatusForbidden, err.Error(), ctx)
 		return
 	}
 
 	if err := ctx.ShouldBindJSON(payload); err != nil {
-		errors.AbortWithError(http.StatusBadRequest, err.Error(), ctx)
+		apierrors.AbortWithError(http.StatusBadRequest, err.Error(), ctx)
 		return
+	}
+
+	location := models.Location{}
+	locationQueryResult := c.DB.Where("id = ?", payload.LocationId).First(&location)
+	if locationQueryResult.Error != nil {
+		if errors.Is(locationQueryResult.Error, gorm.ErrRecordNotFound) {
+			apierrors.AbortWithError(http.StatusBadRequest, fmt.Sprintf("location with id %s does not exist", payload.LocationId), ctx)
+			return
+		} else {
+			log.Err(locationQueryResult.Error).Msg("failed to get location")
+			apierrors.AbortWithError(http.StatusInternalServerError, "failed to get location", ctx)
+			return
+		}
 	}
 
 	now := time.Now()
 	league := models.League{
-		Name:      payload.Name,
-		OwnerID:   currentUser.ID,
-		Slug:      payload.Slug,
-		Location:  payload.Location,
-		CreatedAt: now,
-		UpdatedAt: now,
+		Name:       payload.Name,
+		OwnerID:    currentUser.ID,
+		Slug:       payload.Slug,
+		LocationID: location.ID,
+		CreatedAt:  now,
+		UpdatedAt:  now,
 	}
 
-	result := c.DB.Create(&league)
-	if result.Error != nil {
-		if strings.Contains(result.Error.Error(), "duplicate key value violates unique") {
-			errors.AbortWithError(http.StatusConflict, "league with slug already exists", ctx)
+	createResult := c.DB.Create(&league)
+	if createResult.Error != nil {
+		if strings.Contains(createResult.Error.Error(), "duplicate key value violates unique") {
+			apierrors.AbortWithError(http.StatusConflict, "league with slug already exists", ctx)
 			return
 		} else {
-			log.Err(result.Error).Msg("failed to create league")
-			errors.AbortWithError(http.StatusInternalServerError, "failed to create league", ctx)
+			log.Err(createResult.Error).Msg("failed to create league")
+			apierrors.AbortWithError(http.StatusInternalServerError, "failed to create league", ctx)
 			return
 		}
 	}
 
 	ctx.JSON(http.StatusCreated, generated.LeagueResponse{
 		League: &generated.League{
-			Id:        league.ID.String(),
-			Name:      league.Name,
-			Slug:      league.Slug,
-			Location:  league.Location,
-			OwnerId:   league.OwnerID.String(),
-			CreatedAt: utils.FormatTime(league.CreatedAt),
-			UpdatedAt: utils.FormatTime(league.UpdatedAt),
+			Id:         league.ID.String(),
+			Name:       league.Name,
+			Slug:       league.Slug,
+			LocationId: location.ID.String(),
+			OwnerId:    league.OwnerID.String(),
+			CreatedAt:  utils.FormatTime(league.CreatedAt),
+			UpdatedAt:  utils.FormatTime(league.UpdatedAt),
 		},
 	})
 }
@@ -78,20 +93,20 @@ func (c *Controller) ListLeagues(ctx *gin.Context) {
 	result := c.DB.Find(&dbResults)
 	if result.Error != nil {
 		log.Err(result.Error).Msg("failed to list leagues")
-		errors.AbortWithError(http.StatusInternalServerError, "failed to list leagues", ctx)
+		apierrors.AbortWithError(http.StatusInternalServerError, "failed to list leagues", ctx)
 		return
 	}
 
 	var leagues []generated.League
 	for _, league := range dbResults {
 		leagues = append(leagues, generated.League{
-			Id:        league.ID.String(),
-			Name:      league.Name,
-			Slug:      league.Slug,
-			Location:  league.Location,
-			OwnerId:   league.Owner.ID.String(),
-			CreatedAt: utils.FormatTime(league.CreatedAt),
-			UpdatedAt: utils.FormatTime(league.UpdatedAt),
+			Id:         league.ID.String(),
+			Name:       league.Name,
+			Slug:       league.Slug,
+			LocationId: league.Location.ID.String(),
+			OwnerId:    league.Owner.ID.String(),
+			CreatedAt:  utils.FormatTime(league.CreatedAt),
+			UpdatedAt:  utils.FormatTime(league.UpdatedAt),
 		})
 	}
 
@@ -104,25 +119,25 @@ func (c *Controller) GetLeagueWithSlug(ctx *gin.Context, slug string) {
 	var dbResult models.League
 	result := c.DB.Where("slug = ?", slug).First(&dbResult)
 	if result.Error != nil {
-		if result.Error == gorm.ErrRecordNotFound {
-			errors.AbortWithError(http.StatusNotFound, "league not found", ctx)
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			apierrors.AbortWithError(http.StatusNotFound, "league not found", ctx)
 			return
 		} else {
 			log.Err(result.Error).Msg("failed to get league")
-			errors.AbortWithError(http.StatusInternalServerError, "failed to get league", ctx)
+			apierrors.AbortWithError(http.StatusInternalServerError, "failed to get league", ctx)
 			return
 		}
 	}
 
 	ctx.JSON(http.StatusOK, generated.LeagueResponse{
 		League: &generated.League{
-			Id:        dbResult.ID.String(),
-			Name:      dbResult.Name,
-			Slug:      dbResult.Slug,
-			Location:  dbResult.Location,
-			OwnerId:   dbResult.Owner.ID.String(),
-			CreatedAt: utils.FormatTime(dbResult.CreatedAt),
-			UpdatedAt: utils.FormatTime(dbResult.UpdatedAt),
+			Id:         dbResult.ID.String(),
+			Name:       dbResult.Name,
+			Slug:       dbResult.Slug,
+			LocationId: dbResult.LocationID.String(),
+			OwnerId:    dbResult.OwnerID.String(),
+			CreatedAt:  utils.FormatTime(dbResult.CreatedAt),
+			UpdatedAt:  utils.FormatTime(dbResult.UpdatedAt),
 		},
 	})
 }
